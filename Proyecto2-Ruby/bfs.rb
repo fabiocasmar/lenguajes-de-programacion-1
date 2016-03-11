2c7767510aa6b92e00b2f9267027c42f95292e08
Fabio Castro <fabiocasmar@gmail.com>
2016-03-11 13:51:08 -0430

    subiendo version 0.3, agregando el LCR

--- Proyecto2-Ruby/bfs.rb
+++ Proyecto2-Ruby/bfs.rb
@@ -7,97 +7,155 @@
  	Versión: v0.0.0 
 =end 
 
+require 'set'
+
+module Bfs
+
+  def bfs(start)
+    q        = []
+    q.push(start)
+    visit      = []
+    pushear_nodo = lambda { |x| q.push(x) } 
+    while(q.size != 0)
+      n = q.shift
+      if !visit.include? n
+        n.each pushear_nodo 
+        yield n 
+        visit.push(n)
+      end
+    end
+  end
+
+  def find(start, predicate)
+      start.bfs(start) { |n| return n if predicate.call(n) }
+  end
+
+  def path(start, predicate)
+    f = { start => nil }
+    start.bfs(start) do |n|
+      f.keys.each do |x|
+        if x == n
+          n = x
+        end
+      end
+
+      if predicate.call(n)
+        pa = []
+        while !(n.nil?) do
+          pa.unshift(n)
+          n = f[n]
+        end
+        return pa
+      end
+      n.each lambda { |h| f[h] = n }   
+    end   
+  end
+
+  def walk (start, action)
+    v = []
+    start.bfs(start) do |n| 
+      action.call(n)
+      v.push(n)
+    end 
+    return v
+  end
+
+end
+
 class BinTree
 include Bfs
-	attr_accessor :value,  # Valor almacenado en el nodo
-				  :left,   # BinTree izquierdo
-				  :right   # BinTree derecho
+  attr_accessor :value,  # Valor almacenado en el nodo
+  	            :left,   # BinTree izquierdo
+  	            :right   # BinTree derecho
 	
 	def initialize(v,l,r)
 		@value, @left, @right = v, l, r
 	end
 	
 	def each(b)
-	    b.call(@left)  unless @left.nil?
-	    b.call(@right) unless @right.nil?
+	  b.call(@left)  unless @left.nil?
+	  b.call(@right) unless @right.nil?
 	end
 end
 
 class GraphNode
+include Bfs
 	attr_accessor :value,   # Valor alamacenado en el nodo
-				  :children # Arreglo de sucesores GraphNode
+                :children # Arreglo de sucesores GraphNode
 
 	def initialize(v,c)
 		@value, @children = v, c
 	end
 	
 	def each(b)
-	    @children.each do |c|
-	      b.call(c)
-	    end unless @children.nil?
+	    @children.each { |c| b.call(c) } unless @children.nil?
 	end
 end
 
-module Bfs
-	def bfs(start)
-		q 			 = []
-		q.push(start)
-		visit 		 = []
-		pushear_nodo = lambda { |x| q.push(x) } 
-		while(q.size != 0)
-			n = q.shift
-			if !visit.include? n
-				n.each pushear_nodo 
-				yield n 
-				visit.push(n)
-			end
-		end
-	end
+class LCR
+include Bfs
+  attr_reader :value
+  
+  def initialize(side,left,right)
+  	left.map!  { |c| c.to_sym }
+    right.map! { |c| c.to_sym }
+    @value = {
+      "side" => side.to_sym,
+      "left"  => left,
+      "right" => right
+    }
+  end        
 
-	def find(start, predicate)
-    	start.bfs(start) { |n| return n if predicate.call(n) }
+  def each(p)
+    fun_ver = lambda do |s|
+      if (s.value["side"] == :right)
+      	side_in = "left"
+      else
+      	side_in = "right"
+      end
+      if (s.value[side_in].include?(:lobo) and s.value[side_in].include?(:cabra)) or
+          (s.value[side_in].include?(:cabra) and s.value[side_in].include?(:repollo))
+        false
+      else 
+        true
+      end
     end
 
-    def path(start, predicate)
-    	f = { start => [] }
-    	start.bfs(start) do |n|
-			if predicate.call(n)
-				pa = []
-				while n != [] do
-					pa.unshift(n)
-					n = f[n]
-				end
-				return pa
-			end
-        	n.each lambda { |h| f[h] = n }   
-        end		
-	end
+    if @value["side"] == :left
+        o = "left"
+        d = "right"
+    else
+        o = "right"
+        d = "left"
+    end
 
-	def walk (start, action)
-		v = []
-		start.bfs(start) do |n| 
-			action.call(n)
-			v.push(n)
-		end 
-		return v
-	end
+    @value[o].each do |x|
+        newo = Array.new(@value[o])
+        newo.delete(x)
+        newd = (Array.new(@value[d])).push(x)
+        if o == "right"
+        	ns = LCR.new(d,newd,newo)
+       	else 
+          ns = LCR.new(d,newo,newd)
+       	end
+        p.call(ns) if fun_ver.call(ns)
+    end
 
-end
+    ns = LCR.new(d,@value["left"],@value["right"])
+    p.call(ns) if fun_ver.call(ns)
+  end
 
+  def ==(comp)
+    (@value["side"]           == comp.value["side"]) and
+    (Set.new(@value["right"]) == Set.new(comp.value["right"])) and
+    (Set.new(@value["left"])  == Set.new(comp.value["left"]))
+  end
+  
+  def solve
+    o = LCR.new(:right,[],[:repollo,:cabra,:lobo])
+    p = lambda { |x| x == o }
+    path(self,p).each { |m| puts m.value }
+    return true
+  end
 
-
-def LCR
-	attr_reader :value
-	
-	#def initialize(?) # Indique los argumentos
-	#	# Su c´odigo aqu´ı
-	#end
-	
-	def each(p)
-		# Su c´odigo aqu´ı
-	end
-	
-	def solve
-		# Su c´odigo aqu´ı
-	end
 end
