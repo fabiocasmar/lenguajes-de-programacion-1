f42b5597676bf083b3ceda046c3099be30d7c4d3
Fabio Castro <fabiocasmar@gmail.com>
2016-03-11 19:01:46 -0430

    Arreglando algunos detalles del manejo de errores, agregando una verificacion

--- Proyecto2-Ruby/bfs.rb
+++ Proyecto2-Ruby/bfs.rb
@@ -4,13 +4,13 @@
                    	  Patricia Reinoso    11-10851                                   
  	Organización: Universidad Simón Bolívar                                     
  	Proyecto: Programación Orientada a Objetos - Lenguajes de Programación I                          
- 	Versión: v0.3.0 
+ 	Versión: v0.5.0 
 =end 
 
 module Bfs
 
   def bfs(start)
-    q        = []
+    q          = []
     q.push(start)
     visit      = []
     pushear_nodo = lambda { |x| q.push(x) } 
@@ -25,12 +25,12 @@ module Bfs
   end
 
   def find(start, predicate)
-    raise "start debe tener módulo bfs." if !start.respon_to?bfs
+    raise "start debe tener modulo bfs." unless start.respond_to? :bfs
 	start.bfs(start) { |n| return n if predicate.call(n) } 
   end
 
   def path(start, predicate)
-  	raise "start debe tener módulo bfs." if !start.respon_to?bfs
+  	raise "start debe tener modulo bfs." unless start.respond_to? :bfs
     f = { start => nil }
     start.bfs(start) do |n|
       f.keys.each do |x|
@@ -52,7 +52,7 @@ module Bfs
   end
 
   def walk (start, action)
-  	raise "start debe tener módulo bfs." if !start.respon_to?bfs
+  	raise "start debe tener modulo bfs." unless start.respond_to? :bfs
     v = []
     start.bfs(start) do |n| 
       action.call(n)
@@ -82,7 +82,7 @@ end
 class GraphNode
 include Bfs
 	attr_accessor :value,   # Valor alamacenado en el nodo
-                :children # Arreglo de sucesores GraphNode
+                  :children # Arreglo de sucesores GraphNode
 
 	def initialize(v,c)
 		@value, @children = v, c
@@ -96,38 +96,39 @@ end
 class LCR
 include Bfs
   attr_reader :value
-  
+
+  def fun_ver (s)
+       if (s.value["side"] == :right)
+       	side_in = "left"
+       else
+       	side_in = "right"
+       end
+       if (s.value[side_in].include?(:lobo) and s.value[side_in].include?(:cabra)) or
+           (s.value[side_in].include?(:cabra) and s.value[side_in].include?(:repollo))
+         false
+       else 
+         true
+       end
+  end 
+
   def initialize(side,left,right)
   	left.map!  { |c| c.to_sym }
     right.map! { |c| c.to_sym }
+    side = side.to_sym
     @value = {
-      "side"  => side.to_sym,
+      "side"  => side,
       "left"  => left,
       "right" => right
     }
 
     left.each { |x| raise "Se introdujo una etiqueta erronea en el lado izquierdo." unless ((x ==:lobo)or(x ==:cabra)or(x ==:repollo)) }
     right.each { |x| raise "Se introdujo una etiqueta erronea en el lado derecho." unless ((x ==:lobo)or(x ==:cabra)or(x ==:repollo)) }
-    raise "Se introdujo una etiqueta de lado errónea." unless ((side==:left)or(side==:right))
+    raise "Se introdujo una etiqueta con lado erroneo." unless ((side==:left)or(side==:right))
     raise "Se ha introducido una cantidad de elementos incorrecta" unless (left.length + right.length == 3)
-    raise "Ha introducido un elemento dos veces" unless ((left+right).uniq!)is_nil?
+    raise "Ha introducido un elemento dos veces" unless ((left+right).uniq!).nil?
   end        
 
   def each(p)
-    fun_ver = lambda do |s|
-      if (s.value["side"] == :right)
-      	side_in = "left"
-      else
-      	side_in = "right"
-      end
-      if (s.value[side_in].include?(:lobo) and s.value[side_in].include?(:cabra)) or
-          (s.value[side_in].include?(:cabra) and s.value[side_in].include?(:repollo))
-        false
-      else 
-        true
-      end
-    end
-
     if @value["side"] == :left
         o = "left"
         d = "right"
@@ -145,20 +146,25 @@ include Bfs
        	else 
           ns = LCR.new(d,newo,newd)
        	end
-        p.call(ns) if fun_ver.call(ns)
+        p.call(ns) if fun_ver(ns)
     end
 
     ns = LCR.new(d,@value["left"],@value["right"])
-    p.call(ns) if fun_ver.call(ns)
+    p.call(ns) if fun_ver(ns)
   end
 
   def ==(comp)
-    ((@value["side"]           == comp.value["side"])) and
-    ((@value["right"]).sort()  == (comp.value["right"]).sort()) and
-    ((@value["left"]).sort()   == (comp.value["left"]).sort())
+    if comp.is_a?LCR
+        ((@value["side"]           == comp.value["side"])) and
+        ((@value["right"]).sort()  == (comp.value["right"]).sort()) and
+        ((@value["left"]).sort()   == (comp.value["left"]).sort())
+    else
+		false
+	end
   end
   
   def solve
+  	raise "El estado que ha introducido es invalido" unless self.fun_ver(self)
     o = LCR.new(:right,[],[:repollo,:cabra,:lobo])
     p = lambda { |x| x == o }
     path(self,p).each { |m| puts m.value }
